3acd3d1fafaa83ed1f337d6295510f1d9590e226
Venecio <cristianpessio11@gmail.com>
2022-03-20 21:37:41 -0300

    estructura

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="estilosArticulos.css">
+    <title>Tienda Fito</title>
+
+</head>
+
+<body>
+    <nav class="menuNavegacion">
+        <h3 href="" class="topAviso">Envio varía según su localización</h3>
+        <ul class="UlMenu">
+            <li>Inicio</li>
+            <li>Novedades</li>
+            <h2 class="fitoLogo">Tienda <span class="spanFito">Fito</span></h2>
+            <li>Ofertas</li>
+            <li>Contactame</li>
+        </ul>
+    </nav>
+    <div class="divTabla">
+        <table  style="margin: 0 auto;">
+            <tr>
+                <td>Pantalones</td>
+                <td>Remeras</td>
+                <td>Medias</td>
+                <td>Vestidos</td>
+            </tr>
+            <tr>
+                <td>Ropa interior</td>
+                <td>Calzado</td>
+                <td>Accesorios</td>
+                <td>Camperas</td>
+            </tr>
+            <tr>
+                <td>Camisas</td>
+                <td>Musculosas</td>
+                <td>Conjuntos</td>
+                <td>Hogar</td>
+            </tr>
+
+
+
+
+
+
+
+        </table>
+    </div>
+</body>
+
+</html>
